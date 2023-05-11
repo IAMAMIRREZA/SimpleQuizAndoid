174bb667a570de2caa40fc613edf507c5fe9f32c
IAMAMIRREZA <131593309+IAMAMIRREZA@users.noreply.github.com>
2023-05-11 16:46:53 +0330

    added page 2 and write the question and add submit button!

--- java/com/example/myfirstandroidproject/Page2.java
+++ java/com/example/myfirstandroidproject/Page2.java
@@ -2,7 +2,12 @@ package com.example.myfirstandroidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class Page2 extends AppCompatActivity {
 
@@ -10,5 +15,37 @@ public class Page2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
+
+        RadioButton rbutton=findViewById(R.id.radioButton3);
+
+
+        Button btnsub=findViewById(R.id.button2);
+        btnsub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int TrueAnswer=0;
+                int FalseAnswer=0;
+                int NumberOfTests=0;
+                NumberOfTests++;
+                if (rbutton.isChecked()) {
+                    ++TrueAnswer;
+                    Toast.makeText(Page2.this, "درسته", Toast.LENGTH_SHORT).show();
+                    Intent intentok=new Intent (Page2.this,Page3.class);
+                    intentok.putExtra("true answer",TrueAnswer);
+                    intentok.putExtra("false answer",FalseAnswer);
+                    intentok.putExtra("tests number",NumberOfTests);
+                    startActivity(intentok);
+
+                }   else {
+                    ++FalseAnswer;
+         //           Toast.makeText(Page2.this, "سوال به این راحتی اشتباه نوشتی!!!", Toast.LENGTH_SHORT).show();
+                    Intent intentfalse=new Intent (Page2.this,Page3.class);
+                    intentfalse.putExtra("false answer",FalseAnswer);
+                    intentfalse.putExtra("true answer",TrueAnswer);
+                    intentfalse.putExtra("tests number",NumberOfTests);
+                    startActivity(intentfalse);
+                }
+            }
+        });
     }
 }
